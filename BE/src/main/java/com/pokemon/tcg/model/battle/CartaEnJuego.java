053f38401acbc32f61e6e95d08bb3b853daa2893
Nahuel Gatica <421310@tecnicatura.frc.utn.edu.ar>
2026-04-05 00:44:40 -0300

    merged main into board ui branch [KAN-27]

--- BE/src/main/java/com/pokemon/tcg/model/battle/CartaEnJuego.java
+++ BE/src/main/java/com/pokemon/tcg/model/battle/CartaEnJuego.java
@@ -2,17 +2,21 @@ package com.pokemon.tcg.model.battle;
 
 import com.pokemon.tcg.model.Card;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Wrapper de la carta original que se encuentra en juego.
  */
 public class CartaEnJuego {
+    private Set<String> condicionesEspeciales = new HashSet<>();
     private final Card card; // referencia a la carta original
     private int hpActual;
     private List<Card> energiasUnidas = new ArrayList<>();
     private boolean puedeAtacar = true;
-
+    private int reduccionDanioRecibido = 0;
+    private int aumentoDanioCausado = 0;
     public CartaEnJuego(Card card) {
         this.card = card;
         try {
@@ -29,4 +33,15 @@ public class CartaEnJuego {
     public List<Card> getEnergiasUnidas() { return energiasUnidas; }
     public boolean isPuedeAtacar() { return puedeAtacar; }
     public void setPuedeAtacar(boolean puedeAtacar) { this.puedeAtacar = puedeAtacar; }
+    public Set<String> getCondicionesEspeciales() {
+        return condicionesEspeciales;
+    }
+
+    public void agregarCondicion(String condicion) {
+        this.condicionesEspeciales.add(condicion);
+    }
+
+    public void limpiarCondiciones() {
+        this.condicionesEspeciales.clear();
+    }
 }
