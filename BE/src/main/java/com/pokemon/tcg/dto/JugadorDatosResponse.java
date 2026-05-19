0798292b19288978956898176e6b730a2798264d
Benjamin Polzoni <412056@tecnicatura.frc.utn.edu.ar>
2026-05-19 07:03:56 +0000

    feat: implement internationalization

--- BE/src/main/java/com/pokemon/tcg/dto/JugadorDatosResponse.java
+++ BE/src/main/java/com/pokemon/tcg/dto/JugadorDatosResponse.java
@@ -3,16 +3,49 @@ package com.pokemon.tcg.dto;
 public class JugadorDatosResponse {
     private String username;
     private int sobresDisponibles;
+    private int santoCoins;
     private int cantidadCartas;
+    
+    private String characterId;
+    private String skinColor;
+    private String hairColor;
+    private String eyeColor;
+    private double height;
+    private boolean pikachuCompanion;
 
     public JugadorDatosResponse(String username, int sobresDisponibles, int cantidadCartas) {
         this.username = username;
         this.sobresDisponibles = sobresDisponibles;
+        this.santoCoins = 200;
         this.cantidadCartas = cantidadCartas;
+    }
+
+    public JugadorDatosResponse(String username, int sobresDisponibles, int cantidadCartas,
+                                int santoCoins,
+                                String characterId, String skinColor, String hairColor,
+                                String eyeColor, double height, boolean pikachuCompanion) {
+        this.username = username;
+        this.sobresDisponibles = sobresDisponibles;
+        this.santoCoins = santoCoins;
+        this.cantidadCartas = cantidadCartas;
+        this.characterId = characterId;
+        this.skinColor = skinColor;
+        this.hairColor = hairColor;
+        this.eyeColor = eyeColor;
+        this.height = height;
+        this.pikachuCompanion = pikachuCompanion;
     }
 
     // Getters
     public String getUsername() { return username; }
     public int getSobresDisponibles() { return sobresDisponibles; }
+    public int getSantoCoins() { return santoCoins; }
     public int getCantidadCartas() { return cantidadCartas; }
+
+    public String getCharacterId() { return characterId; }
+    public String getSkinColor() { return skinColor; }
+    public String getHairColor() { return hairColor; }
+    public String getEyeColor() { return eyeColor; }
+    public double getHeight() { return height; }
+    public boolean isPikachuCompanion() { return pikachuCompanion; }
 }
