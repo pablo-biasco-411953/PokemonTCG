aab93fa7c2d3f06c783e797a3d7d034c6cc078af
Benjamin Polzoni <412056@tecnicatura.frc.utn.edu.ar>
2026-05-23 03:19:29 +0000

    chore: update package-lock and deps

--- BE/src/main/java/com/pokemon/tcg/dto/LoginRequest.java
+++ BE/src/main/java/com/pokemon/tcg/dto/LoginRequest.java
@@ -1,8 +1,16 @@
 package com.pokemon.tcg.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class LoginRequest {
+    @NotBlank(message = "El nombre de usuario no puede estar vacío")
     private String username;
+
+    @NotBlank(message = "La contraseña no puede estar vacía")
+    private String password;
 
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
